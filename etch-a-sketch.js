ee7a0a3d4667b0db1223f4461c223b94fd531567
James Couball <jcouball@yahoo.com>
2025-08-15 16:55:40 -0700

    Create different highlighting strategies: black, random, and opacity

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,65 @@
 let cellsWide = 16;
 
+// Could be "black", "random", or "opacity"
+let highlightStrategy = "random";
+
+function randomHexDigit() {
+  const characters = '0123456789abcdef';
+  return characters[Math.floor(Math.random() * characters.length)];
+}
+
+function randomColorInt() {
+  return Math.floor(Math.random() * 256);
+}
+
+function randomColor() {
+  return `${randomColorInt()} ${randomColorInt()} ${randomColorInt()}`;
+}
+
+function highlightBlack(cell, color, opacity) {
+  opacity = 1;
+  cell.setAttribute("data-current-opacity", opacity);
+  let updatedColor = rgbaColor(color, opacity);
+  cell.style.backgroundColor = updatedColor;
+}
+
+function highlightWithOpacity(cell, color, opacity) {
+  if (opacity < 1) {
+    opacity = (Math.floor(opacity * 10) + 1) / 10;
+    cell.setAttribute("data-current-opacity", opacity);
+    let updatedColor = rgbaColor(color, opacity);
+    cell.style.backgroundColor = updatedColor;
+  }
+}
+
+function highlightWithRandomColor(cell, color, opacity) {
+  if (opacity === 0) {
+    color = randomColor();
+    opacity = 1;
+    cell.setAttribute("data-current-color", color);
+    cell.setAttribute("data-current-opacity", opacity);
+    let updatedColor = rgbaColor(color, opacity);
+    console.log(updatedColor);
+    cell.style.backgroundColor = updatedColor;
+  }
+}
+
 function highlightCell(e) {
-  e.target.style.backgroundColor = "black";
+  let cell = e.target;
+  let color = cell.getAttribute('data-current-color');
+  let opacity = Number(cell.getAttribute('data-current-opacity'));
+
+  switch (highlightStrategy) {
+    case "black":
+      highlightBlack(cell, color, opacity);
+      break;
+    case "opacity":
+      highlightWithOpacity(cell, color, opacity);
+      break;
+    case "random":
+      highlightWithRandomColor(cell, color, opacity);
+      break;
+  }
 }
 
 function reset() {
@@ -15,6 +73,10 @@ function reset() {
   }
 }
 
+function rgbaColor(color, opacity) {
+  return `rgba(${color} / ${opacity})`
+}
+
 function resetGrid() {
   newGrid = document.createElement("div");
   newGrid.id = "grid";
@@ -26,6 +88,14 @@ function resetGrid() {
       let cellDiv = document.createElement("div");
       cellDiv.className = "cell";
       cellDiv.addEventListener('mouseover', highlightCell);
+
+      let color = "0 0 0";
+      let opacity = 0;
+      cellDiv.style.backgroundColor = rgbaColor(color, opacity);
+
+      cellDiv.setAttribute('data-current-color', color);
+      cellDiv.setAttribute('data-current-opacity', opacity);
+
       rowDiv.appendChild(cellDiv);
     }
     newGrid.appendChild(rowDiv);
